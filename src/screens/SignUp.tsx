552e8c0407706f628ecca303a6db68b2062773d3
Simon <phillipe.simon@gmail.com>
2024-03-02 09:36:34 -0300

    setting the default background image

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -19,6 +19,7 @@ export function SignUp() {
       <VStack flex={1} bg={"gray.700"} px={10} pb={16}>
         <Image
           source={BackgroundImg}
+          defaultSource={BackgroundImg}
           alt="Pessoas pedalando"
           resizeMode="contain"
           position={"absolute"}
