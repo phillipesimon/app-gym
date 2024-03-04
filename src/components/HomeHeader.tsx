ffafae12260fa770dca5dd2e3c07b6e5fcbe5977
Simon <phillipe.simon@gmail.com>
2024-03-04 05:43:03 -0300

    HomeHeader and userphoto components

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,28 @@
+import { HStack, Heading, Icon, Text, VStack } from "native-base";
+import { UserPhoto } from "./UserPhoto";
+import { TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+export function HomeHeader() {
+  return (
+    <HStack bg={"gray.600"} pt={16} pb={5} px={8} alignItems={"center"}>
+      <UserPhoto
+        source={{ uri: "https://github.com/phillipesimon.png" }}
+        alt="Imagem do usuário"
+        size={16}
+        mr={4}
+      />
+      <VStack flex={1}>
+        <Text color={"gray.100"} fontSize={"md"}>
+          Olá
+        </Text>
+        <Heading color={"gray.100"} fontSize={"md"}>
+          Simon
+        </Heading>
+      </VStack>
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color={"gray.200"} size={7} />
+      </TouchableOpacity>
+    </HStack>
+  );
+}
