a6c2138f62743f9130bd41decb94d34b022040d8
Simon <phillipe.simon@gmail.com>
2024-03-04 06:59:04 -0300

    Exercise list header

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,6 @@
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { Center, FlatList, HStack, Text, VStack } from "native-base";
+import { Center, FlatList, HStack, Heading, Text, VStack } from "native-base";
 import { useState } from "react";
 
 export function Home() {
@@ -29,6 +29,16 @@ export function Home() {
         my={10}
         maxH={10}
       />
+      <VStack flex={1} px={8}>
+        <HStack justifyContent={"space-between"} mb={5}>
+          <Heading color={"gray.200"} fontSize={"md"}>
+            Exercícios
+          </Heading>
+          <Text color={"gray.200"} fontSize={"sm"}>
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
