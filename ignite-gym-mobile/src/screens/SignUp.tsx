e582a6c0347dcb74af971b7108e8cbd5fb2066bb
Simon <phillipe.simon@gmail.com>
2024-03-07 07:10:11 -0300

    ajusting

--- ignite-gym-mobile/src/screens/SignUp.tsx
+++ ignite-gym-mobile/src/screens/SignUp.tsx
@@ -51,7 +51,7 @@ export function SignUp() {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ name, email, password }),
+  https://github.com/phillipesimon/app-gym/watchers    body: JSON.stringify({ name, email, password }),
     });
     const data = await response.json();
     console.log(data);
