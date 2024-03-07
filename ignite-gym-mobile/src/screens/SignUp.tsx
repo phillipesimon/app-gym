9e57feb3358bcc7f7c0f7f7a2463ba1dc0fb5f2f
Simon <phillipe.simon@gmail.com>
2024-03-07 09:22:32 -0300

    HTTP Client com Axios

--- ignite-gym-mobile/src/screens/SignUp.tsx
+++ ignite-gym-mobile/src/screens/SignUp.tsx
@@ -1,14 +1,25 @@
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import { useNavigation } from "@react-navigation/native";
+import {
+  VStack,
+  Image,
+  Text,
+  Center,
+  Heading,
+  ScrollView,
+  useToast,
+} from "native-base";
+import { useForm, Controller } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+
+import { api } from "@services/api";
 
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
-import { useNavigation } from "@react-navigation/native";
-import { Controller, useForm } from "react-hook-form";
+import { AppError } from "@utils/AppError";
 
 type FormDataProps = {
   name: string;
@@ -18,20 +29,20 @@ type FormDataProps = {
 };
 
 const signUpSchema = yup.object({
-  name: yup.string().required("Nome obrigatório"),
-  email: yup.string().email("Email inválido").required("Email obrigatório"),
+  name: yup.string().required("Informe o nome."),
+  email: yup.string().required("Informe o e-mail").email("E-mail inválido."),
   password: yup
     .string()
     .required("Informe a senha")
-    .min(6, "A senha deve conter no minimo 6 digitos"),
+    .min(6, "A senha deve ter pelo menos 6 dígitos."),
   password_confirm: yup
     .string()
-    .required("Confirme a senha")
-    .oneOf([yup.ref("password"), null], "A confirmação de senha não confere"),
+    .required("Confirme a senha.")
+    .oneOf([yup.ref("password"), null], "A confirmação da senha não confere"),
 });
 
 export function SignUp() {
-  const navigation = useNavigation();
+  const toast = useToast();
 
   const {
     control,
@@ -41,47 +52,54 @@ export function SignUp() {
     resolver: yupResolver(signUpSchema),
   });
 
+  const navigation = useNavigation();
+
   function handleGoBack() {
     navigation.goBack();
   }
+
   async function handleSignUp({ name, email, password }: FormDataProps) {
-    const response = await fetch("http://192.168.3.3:3333/users", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name, email, password }),
-    });
-    const data = await response.json();
-    console.log(data);
+    try {
+      const response = await api.post("/users", { name, email, password });
+      console.log(response.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi possível criar a conta. Tente novamente mais tarde.";
+
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
+    }
   }
 
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
       <VStack flex={1} px={10} pb={16}>
         <Image
           source={BackgroundImg}
           defaultSource={BackgroundImg}
-          alt="Pessoas pedalando"
+          alt="Pessoas treinando"
           resizeMode="contain"
-          position={"absolute"}
+          position="absolute"
         />
 
         <Center my={24}>
           <LogoSvg />
-          <Text color={"gray.100"} fontSize={"sm"}>
-            Treine seu corpo e sua mente
+
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo.
           </Text>
         </Center>
 
         <Center>
-          <Heading
-            color={"gray.100"}
-            fontSize={"xl"}
-            mb={6}
-            fontFamily={"heading"}
-          >
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
             Crie sua conta
           </Heading>
 
@@ -97,6 +115,7 @@ export function SignUp() {
               />
             )}
           />
+
           <Controller
             control={control}
             name="email"
@@ -125,15 +144,15 @@ export function SignUp() {
               />
             )}
           />
+
           <Controller
             control={control}
             name="password_confirm"
             render={({ field: { onChange, value } }) => (
               <Input
-                placeholder="Confirmar senha"
+                placeholder="Confirmar a Senha"
                 secureTextEntry
                 onChangeText={onChange}
-                onChange={onChange}
                 value={value}
                 onSubmitEditing={handleSubmit(handleSignUp)}
                 returnKeyType="send"
@@ -147,9 +166,10 @@ export function SignUp() {
             onPress={handleSubmit(handleSignUp)}
           />
         </Center>
+
         <Button
-          title="Voltar para o Login"
-          variant={"outline"}
+          title="Voltar para o login"
+          variant="outline"
           mt={12}
           onPress={handleGoBack}
         />
